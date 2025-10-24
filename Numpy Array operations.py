348158ad52e55b3a4d95ce7c8c030fb2da4c2297
Ishan-student <PU02325EUGBTAR026@student.suas.ac.in>
2025-10-24 15:47:01 +0530

    Numpy library

---
+++ Numpy Array operations.py
@@ -0,0 +1,15 @@
+#Details
+print('Aadarsh Patel')
+print('PU02325EUGBTAR018')
+#Numpy Array operations
+import numpy as np
+arr=np.array([5,4,2])
+arr2=np.array([[5,2,1], [4,5,3]])
+arr3=arr[1]
+arr4=arr2[1,2]
+arr5=arr+arr2[1]
+arr6=arr*arr2[0]
+print(arr3)
+print(arr4)
+print(arr5)
+print(arr6)
